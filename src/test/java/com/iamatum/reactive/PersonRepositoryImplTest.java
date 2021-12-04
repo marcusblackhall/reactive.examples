ed7374aeac5442b59ea4bfdc34b6910657b0a3d7
Marcus Blackhall <marcusinamsterdam@gmail.com>
2021-12-04 15:41:44 +0100

    Add Flux test examples

--- src/test/java/com/iamatum/reactive/PersonRepositoryImplTest.java
+++ src/test/java/com/iamatum/reactive/PersonRepositoryImplTest.java
@@ -1,12 +1,12 @@
 package com.iamatum.reactive;
 
 import com.iamatum.reactive.domain.Person;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
@@ -40,4 +40,31 @@ class PersonRepositoryImplTest {
                 .map(Person::getFirstName)
                 .subscribe(firstName -> System.out.println(firstName));
     }
+
+    @Test
+    void fluxBlockFirst(){
+        Person person = personRepository.findAll().blockFirst();
+        System.out.println(person);
+
+    }
+
+    @Test
+    void testFluxSubscribe(){
+        Flux<Person> fluxPerson = personRepository.findAll();
+        fluxPerson.subscribe(person -> System.out.println(person)
+                );
+
+    }
+
+    @Test
+    void testFluxToMonoList(){
+        Flux<Person> fluxPerson = personRepository.findAll();
+
+        Mono<List<Person>> listMono = fluxPerson.collectList();
+        listMono.subscribe(list ->
+                list.forEach(person -> System.out.println(person))
+                );
+
+    }
+
 }
